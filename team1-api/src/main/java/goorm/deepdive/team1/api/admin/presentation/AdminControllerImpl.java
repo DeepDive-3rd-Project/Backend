3dc68fdb47c41b8255e13cdb6e93800e394a59ed
yohan <rmvmf96@gmail.com>
2025-02-12 02:50:05 +0900

    refacor : 관리자 api 스웨거 추가

--- team1-api/src/main/java/goorm/deepdive/team1/api/admin/presentation/AdminControllerImpl.java
+++ team1-api/src/main/java/goorm/deepdive/team1/api/admin/presentation/AdminControllerImpl.java
@@ -4,6 +4,7 @@ import goorm.deepdive.team1.api.admin.application.AdminFacade;
 import goorm.deepdive.team1.api.admin.presentation.request.AdminRegisterRequest;
 import goorm.deepdive.team1.api.admin.presentation.response.AdminRegisterResponse;
 import goorm.deepdive.team1.api.admin.presentation.response.AdminReissueResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdminControllerImpl implements AdminController{
     private final AdminFacade adminFacade;
 
+    @Override
     @PostMapping("/register")
     public ResponseEntity<AdminRegisterResponse> register(@RequestBody AdminRegisterRequest request) {
         AdminRegisterResponse response = adminFacade.register(request);
@@ -27,12 +29,14 @@ public class AdminControllerImpl implements AdminController{
 
     // JWT 재발급 API
     // 엑세스 토큰이 만료되었을 경우, 401 응답을 반환하고 클라이언트가 /reissue API를 호출한다고 가정
+    @Override
     @PostMapping("/reissue")
     public ResponseEntity<AdminReissueResponse> reissueToken(HttpServletRequest request, HttpServletResponse response) {
         AdminReissueResponse registerResponse = adminFacade.reissueToken(request, response);
         return ResponseEntity.ok(registerResponse);
     }
 
+    @Override
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
         adminFacade.logout(request, response);
