516d5446a68ebaa82c95debbb10fe6240be03848
minjo-on <alswns11346@gmail.com>
2025-02-14 22:58:17 +0900

    Update AddressHistoryRepository.java

--- team1-domain/src/main/java/goorm/deepdive/team1/domain/addresshistory/infrastructure/AddressHistoryRepository.java
+++ team1-domain/src/main/java/goorm/deepdive/team1/domain/addresshistory/infrastructure/AddressHistoryRepository.java
@@ -4,13 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import goorm.deepdive.team1.domain.addresshistory.domain.AddressHistory;
+import goorm.deepdive.team1.domain.addresshistory.domain.AddressHistoryCache;
 
 public interface AddressHistoryRepository {
 	AddressHistory save(AddressHistory addressHistory);
 
 	Optional<AddressHistory> findByIdAndDeletedAtIsNull(Long id);
 
-	List<AddressHistory> findAllByDeletedAtIsNull();
+	List<AddressHistory> findByUserIdAndDeletedAtIsNull(Long userId);
 
 	void delete(AddressHistory addressHistory);
+
+	List<AddressHistoryCache> findCacheByUserIdAndDeletedAtIsNull(Long userId);
+
+	void saveAllCaches(List<AddressHistory> addressHistories);
 }
