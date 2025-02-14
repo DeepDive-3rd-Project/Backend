0c41d1aa1e02caf09da02e09dce8cc112a39d4bc
minjo-on <alswns11346@gmail.com>
2025-02-15 02:10:34 +0900

    refactor: Kafka 메세지 자동 커밋 설정

--- team1-infra/src/main/java/goorm/deepdive/team1/infra/kafka/config/KafkaConsumerConfig.java
+++ team1-infra/src/main/java/goorm/deepdive/team1/infra/kafka/config/KafkaConsumerConfig.java
@@ -12,7 +12,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -32,7 +31,7 @@ public class KafkaConsumerConfig {
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 		props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
-
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,true);
 		return props;
 	}
 
@@ -48,7 +47,6 @@ public class KafkaConsumerConfig {
 		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumer());
 		factory.setConcurrency(3);
-		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
 		factory.setCommonErrorHandler(new DefaultErrorHandler((exception, data) -> {
 			log.error("에러 발생! 잘못된 메시지: {}", data != null ? data.toString() : "데이터 없음", exception);
 		}));
