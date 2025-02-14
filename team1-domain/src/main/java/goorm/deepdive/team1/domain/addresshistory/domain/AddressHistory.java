0726b1254cddcd58e0d5ca63dcad73312f724602
minjo-on <alswns11346@gmail.com>
2025-02-14 22:55:47 +0900

    refactor: userId index 설정

--- team1-domain/src/main/java/goorm/deepdive/team1/domain/addresshistory/domain/AddressHistory.java
+++ team1-domain/src/main/java/goorm/deepdive/team1/domain/addresshistory/domain/AddressHistory.java
@@ -4,14 +4,16 @@ import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-import goorm.deepdive.team1.domain.common.BaseTimeEntity;
 import goorm.deepdive.team1.domain.address.domain.Address;
+import goorm.deepdive.team1.domain.common.BaseTimeEntity;
 import goorm.deepdive.team1.domain.user.domain.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,6 +24,9 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PROTECTED)
+@Table(name = "address_history", indexes = {
+	@Index(name = "idx_user_id", columnList = "user_id")
+})
 public class AddressHistory extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
