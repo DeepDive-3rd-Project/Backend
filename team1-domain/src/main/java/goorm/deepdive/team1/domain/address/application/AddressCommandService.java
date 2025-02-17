936d8bb11d86d79d2b2bf4a513c2c0e5930e38ae
minjo-on <alswns11346@gmail.com>
2025-02-18 01:44:03 +0900

    refactor: 사용하지 않는 api 삭제

--- team1-domain/src/main/java/goorm/deepdive/team1/domain/address/application/AddressCommandService.java
+++ team1-domain/src/main/java/goorm/deepdive/team1/domain/address/application/AddressCommandService.java
@@ -1,7 +1,6 @@
 package goorm.deepdive.team1.domain.address.application;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import goorm.deepdive.team1.domain.address.domain.Address;
 import goorm.deepdive.team1.domain.address.infrastructure.AddressRepository;
@@ -9,42 +8,10 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class AddressCommandService {
 	private final AddressRepository addressRepository;
-	private final KakaoApiAddressService kakaoApiAddressService;
-
-	public Address create(double x, double y, String regionAddress, String roadAddress, String region) {
-		Address address = Address.create(x, y, regionAddress, roadAddress, region);
-		return addressRepository.save(address);
-	}
 
 	public Address save(Address address) {
 		return addressRepository.save(address);
 	}
-
-	public void update(Address address, double x, double y, String regionAddress, String roadAddress, String region) {
-		address.updateX(x);
-		address.updateY(y);
-		address.updateRegionAddress(regionAddress);
-		address.updateRoadAddress(roadAddress);
-		address.updateRegion(region);
-	}
-
-	public void delete(Address address) {
-		address.markAsDeleted();
-	}
-
-	@Transactional
-	public Address findOrCreateAddress(String roadAddress) {
-		return addressRepository.findByRoadAddressContainingAndDeletedAtIsNull(roadAddress)
-			.orElseGet(() -> createAddressFromKakaoApi(roadAddress));
-	}
-
-	private Address createAddressFromKakaoApi(String roadAddress) {
-		Address newAddress = kakaoApiAddressService.getGeoDataFromAddress(roadAddress);
-		return addressRepository.save(newAddress);
-	}
-
-
 }
--- team1-domain/src/main/java/goorm/deepdive/team1/domain/address/application/AddressQueryService.java
+++ team1-domain/src/main/java/goorm/deepdive/team1/domain/address/application/AddressQueryService.java
@@ -1,12 +1,9 @@
 package goorm.deepdive.team1.domain.address.application;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import goorm.deepdive.team1.domain.address.domain.Address;
-import goorm.deepdive.team1.domain.address.exception.AddressNotFoundException;
 import goorm.deepdive.team1.domain.address.infrastructure.AddressRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -15,15 +12,6 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class AddressQueryService {
 	private final AddressRepository addressRepository;
-
-	public Address getById(Long id) {
-		return addressRepository.findByIdAndDeletedAtIsNull(id)
-			.orElseThrow(AddressNotFoundException::new);
-	}
-
-	public List<Address> getAllByDeletedAtIsNull() {
-		return addressRepository.findAllByDeletedAtIsNull();
-	}
 
 	public Address findByRoadAddress(String roadAddress) {
 		return addressRepository.findByRoadAddressContainingAndDeletedAtIsNull(roadAddress)
