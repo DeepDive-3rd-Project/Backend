ffcb935e058a8f79f44f2a314e41fe7d8f620ba9
yohan <rmvmf96@gmail.com>
2025-02-18 15:01:57 +0900

    git 커밋 수정 - 잘못된 커밋입니다  - 파일 작업 되돌렸습니다

--- team1-api/src/main/java/goorm/deepdive/team1/api/config/PropertyConfig.java
+++ team1-api/src/main/java/goorm/deepdive/team1/api/config/PropertyConfig.java
@@ -6,8 +6,7 @@ import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @PropertySources({
-        @PropertySource("file:/app/env.properties") // 배포시 해당 파일 사용
-//        @PropertySource("classpath:env.properties") // env.properties 파일 소스 등록
+        @PropertySource("classpath:env.properties") // env.properties 파일 소스 등록
 })
 public class PropertyConfig {
 
