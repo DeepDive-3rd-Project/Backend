959e050ed4e717a1d5b1b7ccf821efcb3dec85c2
yohan <rmvmf96@gmail.com>
2025-02-18 15:26:10 +0900

    fix :  @propertySource 제거  -  배포 시 해당 어노테이션의 경로의 파일을 못읽는 문제가 있어서 임시로 주석처리

--- team1-api/src/main/java/goorm/deepdive/team1/api/config/PropertyConfig.java
+++ team1-api/src/main/java/goorm/deepdive/team1/api/config/PropertyConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySources({
-        @PropertySource("file:/app/env.properties") // 배포시 해당 파일 사용
+//@PropertySources({
+//        @PropertySource("file:/app/env.properties") // 배포시 해당 파일 사용
 //      @PropertySource("classpath:env.properties") // env.properties 파일 소스 등록// env.properties 파일 소스 등록
-})
+//})
 public class PropertyConfig {
 
 }
